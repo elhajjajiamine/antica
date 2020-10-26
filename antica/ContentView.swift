c32a5bc8bae4ad18f91e9947fab0203e469d520b
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-26 12:57:10 +0100

    Initial Commit

---
+++ antica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  antica
+//
+//  Created by elhajjaji on 10/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ antica/anticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  anticaApp.swift
+//  antica
+//
+//  Created by elhajjaji on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct anticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
